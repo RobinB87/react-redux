00b140389a289d3b9789cb2d9b7f5e1d66908a0e
Robin Bette <robinbette@hotmail.com>
2021-05-28 13:41:41 +0200

    add some dispatches

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ console.log(store);
 
 // you set the state by dispatching actions
 store.dispatch(bugAdded("Bug 1"));
+store.dispatch(bugAdded("Bug 2"));
+store.dispatch(bugAdded("Bug 3"));
 store.dispatch(bugResolved(1));
 
 console.log(store.getState());
