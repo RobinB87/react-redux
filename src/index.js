1e3c625fda8b32bb4c7c0b1100fe6fbd47860633
Robin Bette <robinbette@hotmail.com>
2021-05-28 11:50:07 +0200

    add some comments

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import store from "./store";
 import { bugAdded, bugResolved } from "./actions";
+// there is only a getState method, not a setState
+// so, the setState is actually a private property in this object
+console.log(store);
 
+// you set the state by dispatching actions
 store.dispatch(bugAdded("Bug 1"));
 store.dispatch(bugResolved(1));
 
