8b7cd9dfd4d3b5b29e286e6a223fa5c11b64088d
Robin Bette <robinbette@hotmail.com>
2021-05-28 16:05:46 +0200

    install redux toolkit

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,9 +1,8 @@
-import { createStore } from "redux";
-import { devToolsEnhancer } from "redux-devtools-extension";
+import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./bugs";
 
-// only pass the reference of the reducer, not the function: reducer()
-
-export default function configureStore() {
-  return createStore(reducer, devToolsEnhancer({ trace: true }));
+export default function () {
+  // only pass the reference of the reducer, not the function: reducer()
+  // with redux toolkit you can use their configureStore
+  return configureStore({ reducer });
 }
