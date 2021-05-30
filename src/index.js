9233ff0d91404ae1d82c54ff42d9a8fab1019eb3
Robin Bette <robinbette@hotmail.com>
2021-05-30 11:10:59 +0200

    add middleware

--- src/index.js
+++ src/index.js
@@ -12,20 +12,20 @@ console.log(store);
 // you set the state by dispatching actions
 // with redux toolkit, you want to pass an object
 // store.dispatch(bugAdded("Bug 1")); // so, not pass this string, but:
-store.dispatch(bugAdded({ description: "Bug 1" }));
-store.dispatch(bugAdded({ description: "Bug 2" }));
-store.dispatch(bugAdded({ description: "Bug 3" }));
-store.dispatch(bugResolved({ id: 1 }));
-store.dispatch(projectAdded({ name: "Project1" }));
+// store.dispatch(bugAdded({ description: "Bug 1" }));
+// store.dispatch(bugAdded({ description: "Bug 2" }));
+// store.dispatch(bugAdded({ description: "Bug 3" }));
+// store.dispatch(bugResolved({ id: 1 }));
+// store.dispatch(projectAdded({ name: "Project1" }));
 
-const unresolvedBugs = getUnresolvedBugs(store.getState());
-console.log(unresolvedBugs);
+// const unresolvedBugs = getUnresolvedBugs(store.getState());
+// console.log(unresolvedBugs);
 
 store.dispatch(userAdded({ name: "Blik-Jan" }));
-store.dispatch(userAdded({ name: "Henkie" }));
-store.dispatch(userAdded({ name: "De Padde" }));
+// store.dispatch(userAdded({ name: "Henkie" }));
+// store.dispatch(userAdded({ name: "De Padde" }));
 
-store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
+// store.dispatch(bugAssignedToUser({ bugId: 1, userId: 1 }));
 
 // pass the userId 1, this will return a func, which will get the state
 // and it will return the bugs assigned to that user
--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,8 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import reducer from "./reducer";
+import logger from "./middleware/logger";
 
 export default function () {
   // only pass the reference of the reducer, not the function: reducer()
   // with redux toolkit you can use their configureStore
-  return configureStore({ reducer });
+  return configureStore({ reducer, middleware: [logger({ destination: "console" })] });
 }
