603ad2da84a1b2be8ca5a0f5dcbc927d81522e92
Robin Bette <robinbette@hotmail.com>
2021-05-31 10:29:42 +0200

    implement action creator

--- src/store/middleware/api.js
+++ src/store/middleware/api.js
@@ -1,10 +1,11 @@
 import axios from "axios";
+import * as actions from "../api";
 
 const api =
   ({ dispatch }) =>
   (next) =>
   async (action) => {
-    if (action.type !== "apiCallBegan") return next(action);
+    if (action.type !== actions.apiCallBegan.type) return next(action);
 
     next(action);
     const { url, method, data, onSuccess, onError } = action.payload;
@@ -17,9 +18,17 @@ const api =
         method,
         data,
       });
-      dispatch({ type: onSuccess, payload: response.data });
+      // general
+      dispatch(actions.apiCallSuccess(response.data));
+
+      // specific
+      if (onSuccess) dispatch({ type: onSuccess, payload: response.data });
     } catch (error) {
-      dispatch({ type: onError, payload: error });
+      // general error action
+      dispatch(actions.apiCallFailed(error));
+
+      // specific
+      if (onError) dispatch({ type: onError, payload: error });
     }
   };
 
