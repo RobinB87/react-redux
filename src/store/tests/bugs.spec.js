8f0ce9a9feb9363eafe3fe63bb47fd7721588c06
Robin Bette <robinbette@hotmail.com>
2021-06-01 09:53:04 +0200

    mock store

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -1,13 +1,21 @@
+import axios from "axios";
+import MockAdapter from "axios-mock-adapter";
 import { addBug } from "../bugs";
 import configureStore from "../configureStore";
 
 describe("bugSlice", () => {
   it("should handle the addBug action", async () => {
     // dispatch addbug => store (is it inside?, do not care how it happens)
-    const store = configureStore();
-    const bug = { description: "a" };
-    await store.dispatch(addBug(bug));
 
-    expect(store.getState().entities.bugs.list).toHaveLength(1);
+    const bug = { description: "a" };
+    const savedBug = { ...bug, id: 1 };
+
+    const fakeAxios = new MockAdapter(axios);
+    // response code (200) and request body
+    fakeAxios.onPost("/bugs").reply(200, savedBug);
+
+    const store = configureStore();
+    await store.dispatch(addBug(bug));
+    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
   });
 });
