62417b4058408899c69e582ca854f6abdfcf0dfa
Robin Bette <robinbette@hotmail.com>
2021-06-01 10:02:05 +0200

    write clean tests

--- src/store/tests/bugs.spec.js
+++ src/store/tests/bugs.spec.js
@@ -4,18 +4,38 @@ import { addBug } from "../bugs";
 import configureStore from "../configureStore";
 
 describe("bugSlice", () => {
-  it("should handle the addBug action", async () => {
+  let fakeAxios;
+  let store;
+
+  // function which takes a func as an argument and executes before each test
+  beforeEach(() => {
+    fakeAxios = new MockAdapter(axios);
+    store = configureStore();
+  });
+
+  const bugSlice = () => store.getState().entities.bugs;
+
+  it("should add the bug to the store if it is saved to the server", async () => {
     // dispatch addbug => store (is it inside?, do not care how it happens)
 
     const bug = { description: "a" };
     const savedBug = { ...bug, id: 1 };
-
-    const fakeAxios = new MockAdapter(axios);
     // response code (200) and request body
     fakeAxios.onPost("/bugs").reply(200, savedBug);
 
-    const store = configureStore();
     await store.dispatch(addBug(bug));
-    expect(store.getState().entities.bugs.list).toContainEqual(savedBug);
+
+    expect(bugSlice().list).toContainEqual(savedBug);
+  });
+
+  it("should not add the bug to the store if it is not saved to the server", async () => {
+    // dispatch addbug => store (is it inside?, do not care how it happens)
+
+    const bug = { description: "a" };
+    fakeAxios.onPost("/bugs").reply(500);
+
+    await store.dispatch(addBug(bug));
+
+    expect(bugSlice().list).toHaveLength(0);
   });
 });
